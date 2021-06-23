e84b33b02d71b483f16388c8765fe288afc0686b
di-ops <vekshin8@gmail.com>
2021-06-23 14:37:12 +0300

    Add author for msgs

--- src/main/java/com/myCompany/sweater/domain/Message.java
+++ src/main/java/com/myCompany/sweater/domain/Message.java
@@ -1,9 +1,6 @@
 package com.myCompany.sweater.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Message {
@@ -11,12 +8,31 @@ public class Message {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
 
+    private String text;
+    private String tag;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
+
+
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
         this.text = text;
         this.tag = tag;
+        author = user;
+    }
+
+    public String getAuthorName(){
+        return author != null? author.getUsername(): "/none/";
+    }
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 
     public String getText() {
@@ -42,7 +58,4 @@ public class Message {
     public void setId(Integer id) {
         this.id = id;
     }
-
-    private String text;
-    private String tag;
 }
